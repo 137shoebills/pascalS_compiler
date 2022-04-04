bf7107b79b2cf002a0dd55421175e64bf9ae00a5
布苓苓苓 <80634381+Brynn666@users.noreply.github.com>
2022-04-04 11:20:43 +0800

    Add files via upload

---
+++ Code/2SyntaxAnalysis/yacc.tab.h
@@ -0,0 +1,114 @@
+/* A Bison parser, made by GNU Bison 3.0.4.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_YACC_TAB_H_INCLUDED
+# define YY_YY_YACC_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    PROGRAM = 258,
+    CONST = 259,
+    VAR = 260,
+    ARRAY = 261,
+    OF = 262,
+    PROCEDURE = 263,
+    FUNCTION = 264,
+    BEGIN = 265,
+    END = 266,
+    IF = 267,
+    THEN = 268,
+    CASE = 269,
+    FOR = 270,
+    TO = 271,
+    DOWNTO = 272,
+    DO = 273,
+    ELSE = 274,
+    REPEAT = 275,
+    UNTIL = 276,
+    WHILE = 277,
+    IDENTIFIER = 278,
+    UINUM = 279,
+    UFNUM = 280,
+    CHAR = 281,
+    LETTER = 282,
+    INTEGER = 283,
+    TYPE = 284,
+    ASSIGNOP = 285,
+    RELOP = 286,
+    ADDOP = 287,
+    MULOP = 288,
+    NOT = 289,
+    RECORD = 290,
+    REAL = 291,
+    BOOLEAN = 292,
+    ADD = 293,
+    MUL = 294
+  };
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+/* Location type.  */
+#if ! defined YYLTYPE && ! defined YYLTYPE_IS_DECLARED
+typedef struct YYLTYPE YYLTYPE;
+struct YYLTYPE
+{
+  int first_line;
+  int first_column;
+  int last_line;
+  int last_column;
+};
+# define YYLTYPE_IS_DECLARED 1
+# define YYLTYPE_IS_TRIVIAL 1
+#endif
+
+
+extern YYSTYPE yylval;
+extern YYLTYPE yylloc;
+int yyparse (void);
+
+#endif /* !YY_YY_YACC_TAB_H_INCLUDED  */
