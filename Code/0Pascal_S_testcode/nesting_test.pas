1dd746d2a1a56ec4db3f41c60c8772463e7f49b3
Nimloth D <57124280+lxl23333@users.noreply.github.com>
2022-04-14 14:55:05 +0800

    pascal函数/过程嵌套示例

---
+++ Code/0Pascal_S_testcode/nesting_test.pas
@@ -0,0 +1,61 @@
+program main;
+var a, b, ret: integer;     {全局变量}
+
+procedure test(); 
+    procedure inner();
+	    procedure inner1(a,b:integer;var ret:integer);
+		    function func(a,b:integer):integer;
+		    begin
+			    writeln('func');
+			    func:=a+b;
+		    end;    { end of function func }  
+		
+	    {inner1}
+	    begin
+			writeln('inner1');
+			a:=1;
+			b:=2;
+			a:=func(a,b);
+			ret:=a+b;
+	    end;    { end of procedure inner1 }  
+	
+    	(*不支持跨过程调用
+	    procedure inner2(x,y:integer;var ret:integer);
+	    begin
+		    writeln(func(a,b));
+	    end;
+	    *)
+	
+	    procedure noPara;
+	    begin
+		    writeln('noPara');
+		end;    { end of procedure noPara }  
+
+    {inner}
+    begin
+		writeln('inner');
+		inner1(a,b,ret);
+		(*writeln(func(a,b));*) {不支持隔层调用：func属于inner1，inner不能调用}
+		noPara;
+    end;    { end of procedure inner }  
+	
+{test}
+begin
+	inner();
+end;    { end of procedure test }  
+
+{main}
+begin
+	a:=10;
+	b:=20;
+   test();
+   writeln('ret=',ret);
+end.
+
+(*output:
+inner
+inner1
+func
+noPara
+ret=5   {使用了局部变量的值}
+*)
