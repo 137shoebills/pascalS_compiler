a1e9f83d959e0db3a06b1231799a754bbb730be6
Nimloth D <57124280+lxl23333@users.noreply.github.com>
2022-04-16 13:19:46 +0800

    Update nesting_test.pas
    对照Pascal_S语法修正了一些细节

--- Code/0Pascal_S_testcode/nesting_test.pas
+++ Code/0Pascal_S_testcode/nesting_test.pas
@@ -1,22 +1,20 @@
-program main;
+program main(x);	    {Pascal_S语法：program IDENTIFIER(IDENTIFIER,...);}
 var a, b, ret: integer;     {全局变量}
 
-procedure test(); 
-    procedure inner();
+procedure test;			{无参过程：省略括号}
+    procedure inner;
 	    procedure inner1(a,b:integer;var ret:integer);
 		    function func(a,b:integer):integer;
 		    begin
-			    writeln('func');
 			    func:=a+b;
 		    end;    { end of function func }  
 		
 	    {inner1}
 	    begin
-		    writeln('inner1');
-		    a:=1;
-		    b:=2;
-		    a:=func(a,b);
-		    ret:=a+b;
+			a:=1;
+			b:=2;
+			a:=func(a,b);
+			ret:=a+b;
 	    end;    { end of procedure inner1 }  
 	
     	    (*不支持跨过程调用
@@ -28,34 +26,24 @@ procedure test();
 	
 	    procedure noPara;
 	    begin
-		    writeln('noPara');
+		    writeln('n');
 	    end;    { end of procedure noPara }  
 
     {inner}
     begin
-	    writeln('inner');
-	    inner1(a,b,ret);
-	    (*writeln(func(a,b));*) {不支持隔层调用：func属于inner1，inner不能调用}
-	    noPara;
+		inner1(a,b,ret);
+		(*writeln(func(a,b));*) {不支持隔层调用：func属于inner1，inner不能调用}
+		noPara;
     end;    { end of procedure inner }  
 	
 {test}
 begin
-	inner();
+	inner;
 end;    { end of procedure test }  
 
 {main}
 begin
-    a:=10;
-    b:=20;
-    test();
-    writeln('ret=',ret);
+	a:=10;
+	b:=20;
+        test;
 end.
-
-(*output:
-inner
-inner1
-func
-noPara
-ret=5   {使用了局部变量的值}
-*)
