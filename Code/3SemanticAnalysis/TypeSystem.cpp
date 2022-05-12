4dd88fa825abd480d87947d2a1a2f5be11e079eb
Nimloth D <57124280+lxl23333@users.noreply.github.com>
2022-05-13 00:17:59 +0800

    更新record相关函数

--- Code/3SemanticAnalysis/TypeSystem.cpp
+++ Code/3SemanticAnalysis/TypeSystem.cpp
@@ -23,41 +23,31 @@ llvm::Type* TypeSystem::getllType(string type){
     return nullptr;
 }
 
-/* //判断类型转换合法性
-bool TypeSystem::isCastLegal(string fromType, string toType) {
-    vector<string> table = castTable[fromType];
-    auto it = find(table.begin(), table.end(), toType);
-    if(it != table.end())
-        return true;
-    return false;
-} */
-
-/* //新增record对应的LLVM类型
+ //新增record对应的LLVM类型
 void TypeSystem::addRecordType(string name, llvm::StructType* type){
     recordTypes[name] = type;
     recordMembers[name] = vector<NameType>();
 }
 
 //新增record成员
-void TypeSystem::addRecordMember(string recName, string memName, string memType){
+void TypeSystem::addRecordMember(string recName, int lineNo, string memName, string memType){
     if(recordTypes.find(recName) == recordTypes.end()){
         //报错：未定义的record类型
+        semanticErrorInformation.push_back((string) "line:" + char('0' + lineNo) + "Error: Undefined record type: " + recName);
     }
     recordMembers[recName].push_back(make_pair(memName, memType));
 }
 
-//获取record中成员所在的位置（第几个）
+
+//获取record中成员所在的位置下标
 long TypeSystem::getRecordMemberIndex(string recName, string memName){
-    if(recordTypes.find(recName) == recordTypes.end()){
-        //报错：未定义的record类型
-    }
     auto& members = recordMembers[recName];
     for(auto it=members.begin(); it!=members.end(); it++)
     {
         if(it->second == memName)
             return distance(members.begin(), it);
     }
-    
     //报错：未知的record成员
+    semanticErrorInformation.push_back("Error: Undefined record member: " + recName + "." + memName);
     return 0;
-} */
+}
