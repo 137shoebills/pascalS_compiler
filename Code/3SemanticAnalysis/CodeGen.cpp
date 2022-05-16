a6cc33bb4740e5aa8e18d4e2b661f360600fa6be
Nov29 <2931485055@qq.com>
2022-05-16 18:34:14 +0800

    cogen2

--- Code/3SemanticAnalysis/CodeGen.cpp
+++ Code/3SemanticAnalysis/CodeGen.cpp
@@ -170,7 +170,92 @@ llvm::Value* _VariantReference::codeGen(){
 }
 
 llvm::Value* _Expression::codeGen(){
+      llvm::Value* ret;
+      if(this->type == "integer"){
+          ret = llvm::ConstantInt::get(llvm::Type::getInt32Ty(context.llvmContext), this->intValue, true);
+      }
+      else if(this->type == "real"){
+          ret = llvm::ConstantFP::get(llvm::Type::getDoubleTy(context.llvmContext), this->realValue, true);
+      }
+      else if(this->type == "char"){
+          ret = llvm::ConstantInt::get(llvm::Type::getInt8Ty(context.llvmContext), this->charValue, true);
+      }
+      else if(this->type == "boolean"){
+          ret = llvm::ConstantInt::get(llvm::Type::getInt1Ty(context.llvmContext), this->boolValue, true);
+      }
+      else if(this->type=="var"){
+          //!!!!!!!!!
+          int loc = mainSymbolTable->idToLoc[this->variantReference->variantId.first].top();
+          ret = mainSymbolTable->recordList[loc]->llValue;
+      }
+      else if(this->type=="function"){
+          string funId = this->functionCall->functionId.first;
+          int loc = mainSymbolTable->idToLoc[funId].top();
+          ret = mainSymbolTable->recordList[loc]->llValue;
+      }
+      else if(this->type=="compound" && this->operation="bracket"){
+          ret this->operand1->codeGen();
+      }
+      else if(this->type=="compound" && this->operation="not"){
+          ret = llvm::ConstantInt::get(llvm::Type::getInt1Ty(context.llvmContext), !(this->operand1->llValue->getValue()), true);  
+      }
+      else if(this->type==_expression->operation="minus"){
+          ret = llvm::ConstantInt::get(llvm::Type::getInt1Ty(context.llvmContext), -(this->operand1->llValue->getValue()), true);
+      }
+      else if(this->type="compound"){
+          if(this->expressionType != "error"){
+              Value* L = this->operand1->codeGen();
+              Value* R = this->operand2->codeGen();
+              bool fp = false;
 
+              if( (L->getType()->getTypeID() == Type::DoubleTyID) || (R->getType()->getTypeID() == Type::DoubleTyID) ){  // type upgrade
+                  fp = true;
+                  if( (R->getType()->getTypeID() != Type::DoubleTyID) ){
+                      R = context.builder.CreateUIToFP(R, Type::getDoubleTy(context.llvmContext), "ftmp");
+                  }
+                  if( (L->getType()->getTypeID() != Type::DoubleTyID) ){
+                      L = context.builder.CreateUIToFP(L, Type::getDoubleTy(context.llvmContext), "ftmp");
+                  }
+              }
+              if( !L || !R ){
+              return nullptr;
+              }
+              if(this->operation == "+")
+                  ret = fp ? context.builder.CreateFAdd(L, R, "addftmp") : context.builder.CreateAdd(L, R, "addtmp");
+              else if(this->operation == "-")
+                  ret = fp ? context.builder.CreateFSub(L, R, "subftmp") : context.builder.CreateSub(L, R, "subtmp");
+              else if(this->operation == "*")
+                  ret = fp ? context.builder.CreateFMul(L, R, "mulftmp") : context.builder.CreateMul(L, R, "multmp");
+              else if(this->operation == "/")
+                  ret = fp ? context.builder.CreateFDiv(L, R, "divftmp") : context.builder.CreateSDiv(L, R, "divtmp");
+              else if(this->operation == "and")
+                  ret = fp ? LogErrorV("Double type has no AND operation") : context.builder.CreateAnd(L, R, "andtmp");
+              else if(this->operation == "or")
+                  ret = fp ? LogErrorV("Double type has no OR operation") : context.builder.CreateOr(L, R, "ortmp");
+              else if(this->operation == "div"){
+                  ret = llvm::ConstantInt::get(llvm::Type::getInt32Ty(context.llvmContext), int(L->getValue()/R->getValue()), true);
+              }
+              else if(this->operation == "mod")
+                  ret = fp ? LogErrorV("Double type has no Mod operation") : context.builder.CreateSRem(L, R, "modtmp");
+              else if(this->operation == ">=")
+                  ret = fp ? context.builder.CreateFCmpOGE(L, R, "cmpftmp") : context.builder.CreateICmpSGE(L, R, "cmptmp");
+              else if(this->operation == ">")
+                  ret = fp ? context.builder.CreateFCmpOGT(L, R, "cmpftmp") : context.builder.CreateICmpSGT(L, R, "cmptmp");
+              else if(this->operation == "<=")
+                  ret = fp ? context.builder.CreateFCmpOLE(L, R, "cmpftmp") : context.builder.CreateICmpSLE(L, R, "cmptmp");    
+              else if(this->operation == "<")
+                  ret = fp ? context.builder.CreateFCmpOLT(L, R, "cmpftmp") : context.builder.CreateICmpSLT(L, R, "cmptmp");
+              else if(this->operation == "<>")
+                  ret = fp ? context.builder.CreateFCmpONE(L, R, "cmpftmp") : context.builder.CreateICmpNE(L, R, "cmptmp");
+              else if(this->operation == "=")
+                  ret = fp ? context.builder.CreateFCmpOEQ(L, R, "cmpftmp") : context.builder.CreateICmpEQ(L, R, "cmptmp");
+              else
+                  ret = LogErrorV("Unknown binary operator");
+
+          }
+      }
+      this->llValue == ret;
+      return ret;
 }
 
 llvm::Value* _FunctionCall::codeGen(){
@@ -251,19 +336,131 @@ llvm::Value* _AssignStatement::codeGen(){
 }
 
 llvm::Value* _IfStatement::codeGen(){
-    
+      llvm::Value* condValue = this->condition->codeGen();
+      if( !condValue )
+          return nullptr;
+      condValue = CastToBoolean(context, condValue);
+      llvm::Function* theFunction = context.builder.GetInsertBlock()->getParent();//得到if语句所属函数
+      llvm::BasicBlock *thenBB = BasicBlock::Create(context.llvmContext, "then", theFunction);
+      llvm::BasicBlock *elsBB = BasicBlock::Create(context.llvmContext, "else");//else部分
+      llvm::BasicBlock *mergeBB = BasicBlock::Create(context.llvmContext, "ifcont");
+      if( this->els ){
+        //condvalue为条件判断结果值。如果为true，就进入thenBB分支，如果为false，就进入elsBB分支。
+          context.builder.CreateCondBr(condValue, thenBB, elsBB);
+      } else{
+          context.builder.CreateCondBr(condValue, thenBB, mergeBB);
+      }
+      //插入指令
+      context.builder.SetInsertPoint(thenBB);
+      context.pushBlock(thenBB);
+      this->then->codeGen();
+      context.popBlock();
+      thenBB = context.builder.GetInsertBlock();
+      if( thenBB->getTerminator() == nullptr ){
+          context.builder.CreateBr(mergeBB);
+      }
+      if( this->els ){
+          theFunction->getBasicBlockList().push_back(elsBB);
+          context.builder.SetInsertPoint(elsBB);
+          context.pushBlock(thenBB);
+          this->els->codeGen();
+          context.popBlock();
+          context.builder.CreateBr(mergeBB);
+      }
+      theFunction->getBasicBlockList().push_back(mergeBB);
+      context.builder.SetInsertPoint(mergeBB);
+      return nullptr;
 }
 
 llvm::Value* _ForStatement::codeGen(){
+      llvm::Function* theFunction = context.builder.GetInsertBlock()->getParent();
+      llvm::BasicBlock *block = BasicBlock::Create(context.llvmContext, "forloop", theFunction);
+      llvm::BasicBlock *after = BasicBlock::Create(context.llvmContext, "forcont");
 
+      // execute the initial
+      if( this->initial )
+          this->initial->codeGen(context);
+
+      llvm::Value* condValue = this->condition->codeGen();
+      if( !condValue )
+          return nullptr;
+
+      condValue = CastToBoolean(context, condValue);
+
+      // fall to the block
+      context.builder.CreateCondBr(condValue, block, after);
+
+      context.builder.SetInsertPoint(block);
+
+      context.pushBlock(block);
+
+      this->_do->codeGen(context);
+
+      context.popBlock();
+
+      // do increment
+      if( this->increment ){
+          this->increment->codeGen(context);
+      }
+
+      // execute the again or stop
+      condValue = this->condition->codeGen(context);
+      condValue = CastToBoolean(context, condValue);
+      context.builder.CreateCondBr(condValue, block, after);
+
+      // insert the after block
+      theFunction->getBasicBlockList().push_back(after);
+      context.builder.SetInsertPoint(after);
+
+      return nullptr;
 }
 
 llvm::Value* _WhileStatement::codeGen(){
-    //应该和_ForStatement类似
+      //应该和_ForStatement类似
+      llvm::Function* theFunction = context.builder.GetInsertBlock()->getParent();
+  	llvm::BasicBlock *block = BasicBlock::Create(context, "while_body", sum_fun);
+  	llvm::BasicBlock *after = BasicBlock::Create(context, "while_end", sum_fun);
+      llvm::Value* condValue = this->condition->codeGen(context);
+      if( !condValue )
+          return nullptr;
+
+      condValue = CastToBoolean(context, condValue);
+      // fall to the block
+      context.builder.CreateCondBr(condValue, block, after);
+      context.builder.SetInsertPoint(block);
+      context.pushBlock(block);
+      this->_do->codeGen(context);
+      context.popBlock();
+      // execute the again or stop
+      condValue = this->condition->codeGen(context);
+      condValue = CastToBoolean(context, condValue);
+      context.builder.CreateCondBr(condValue, block, after);
+      // insert the after block
+      theFunction->getBasicBlockList().push_back(after);
+      context.builder.SetInsertPoint(after)
+      return nullptr;
 }
 
 llvm::Value* _RepeatStatement::codeGen(){
-    //应该和_ForStatement类似
+      //应该和_ForStatement类似
+      llvm::Function* theFunction = context.builder.GetInsertBlock()->getParent();
+  	llvm::BasicBlock *block = llvm::BasicBlock::Create(context, "while_body", sum_fun);
+  	llvm::BasicBlock *after = llvm::BasicBlock::Create(context, "while_end", sum_fun);
+
+      // fall to the block
+      context->builder.CreateBr(block);
+      context.builder.SetInsertPoint(block);
+      context.pushBlock(block);
+      this->_do->codeGen(context);
+      context.popBlock();
+      // execute the again or stop
+      condValue = this->condition->codeGen(context);
+      condValue = CastToBoolean(context, condValue);
+      context.builder.CreateCondBr(condValue, block, after);
+      // insert the after block
+      theFunction->getBasicBlockList().push_back(after);
+      context.builder.SetInsertPoint(after)
+      return nullptr;
 }
 
 llvm::Value* _Idvpart::codeGen(){
