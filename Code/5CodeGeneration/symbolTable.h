13f473f5e924d1e0c2094360e4934759163e1a17
Nimloth D <57124280+lxl23333@users.noreply.github.com>
2022-05-18 12:04:41 +0800

    Update symbolTable.h
    同步更新语义分析代码

--- Code/5CodeGeneration/symbolTable.h
+++ Code/5CodeGeneration/symbolTable.h
@@ -18,8 +18,9 @@ class _SymbolTable;
 class _SymbolRecord
 {
 public:
-	string flag; //"value parameter"表示传值参数,"var parameter"表示传引用参数,"normal variant"表示普通变量,"constant"表示常量,
-	//"array"表示数组,"procedure"表示过程,"function"表示函数
+	string flag; //"value parameter"表示传值参数,"var parameter"表示传引用参数,"constant"表示常量,
+	//"array"表示数组,"procedure"表示过程,"function"表示函数,"record"表示record集
+	//"normal variant"表示普通变量 或者 自定义类型 或者record类型
 	//"program"表示该条记录是主程序
 	//"parameter of program"表示主程序的参数
 	string id;		//表示标识符名称或者程序函数过程名
@@ -33,7 +34,7 @@ public:
 	//如果是数组，则表示数组维数，如果是函数/过程，则表示参数个数
 	vector<pair<int, int> > arrayRangeList; //数组各维上下界
 	vector<_SymbolRecord *> records;		//记录records中各个记录
-	vector<_FormalParameter*> paras;	//function的形参列表  为什么procedure没有记录此项？？？
+	vector<_FormalParameter*> paras;	//function的形参列表 
 
 	llvm::Value* llValue;		//LLVM类型值
 
@@ -49,6 +50,7 @@ public:
 	// void setSubProgram(string id, int lineNumber,int amount, string returnType);
 	void setVoidPara(string id, int lineNumber);	//当前符号表所在的程序 中的参数
 	void setRecords(string id, int lineNumber, vector<_SymbolRecord *> records);
+	void setCustom(string id, int lineNumber, string type); //增加自定义类型
 
 	bool checkArrayXthIndexRange(int X, int index); //检查第X维下标是否越界，true表示越界，false表示未越界
 	string findXthFormalParaType(int X); //找到第X个形式参数的类型
@@ -82,6 +84,7 @@ public:
 	void addVoidPara(string id, int lineNumber);															   //表示主程序的参数
 	void addRecords(string id, int lineNumber, vector<_SymbolRecord *> records);							   //增加record类型
 	void addProgram(string id, int lineNumber, int amount, string returnType);
+	void addCustom(string id, int lineNumber, string type); //增加自定义类型
 	// void addSubProgram(string id, int lineNumber,int amount, string returnType);
 
 
