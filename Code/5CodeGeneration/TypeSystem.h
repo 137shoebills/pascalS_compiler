eaced596f569925cbbe6ec55af19c2f7c3bee501
Nimloth D <57124280+lxl23333@users.noreply.github.com>
2022-05-19 19:06:54 +0800

    add 数组变量对应的LLVM Type；getVarllType()

--- Code/5CodeGeneration/TypeSystem.h
+++ Code/5CodeGeneration/TypeSystem.h
@@ -20,7 +20,9 @@ class TypeSystem{
 private:
     llvm::LLVMContext& llvmContext;
     map<string, vector<NameType>> recordMembers;    //record列表
-    map<string, llvm::StructType*> recordTypes;     //record名字对应的LLVM类型
+    map<string, llvm::StructType*> recordTypes;     //record类型的名称对应的LLVM Type
+
+    map<string, llvm::ArrayType*> arrayTypes;       //数组变量对应的LLVM Type
 
 public:
     llvm::Type* intTy = llvm::Type::getInt32Ty(llvmContext);
@@ -28,6 +30,7 @@ public:
     llvm::Type* charTy = llvm::Type::getInt8Ty(llvmContext);
     llvm::Type* boolTy = llvm::Type::getInt1Ty(llvmContext);
     llvm::Type* voidTy = llvm::Type::getVoidTy(llvmContext);
+    llvm::Type* int64Ty = llvm::Type::getInt64Ty(llvmContext);
 
     TypeSystem(llvm::LLVMContext& context);
 
@@ -40,6 +43,12 @@ public:
     void addRecordMember(string recName, int lineNo, string memName, string memType);
     //获取record中成员所在的位置（第几个）（处理record成员/record成员赋值时）
     long getRecordMemberIndex(string recName, string memName);
+
+    //新增数组变量对应的LLVM类型
+    void addArrayType(string name, llvm::ArrayType* type);
+
+    //获取变量对应的LLVM Type(普通变量，数组元素，record成员，函数标识符)
+    llvm::Type* getVarllType(_VariantReference* varRef);
 }
 
 #endif
