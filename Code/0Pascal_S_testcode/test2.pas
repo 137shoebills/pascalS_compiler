653a74a8a98dad09d0adaa4d98ed82e7e5ec206c
zsy <57083234+victoria-code@users.noreply.github.com>
2022-05-20 01:43:09 +0800

    Add files via upload

---
+++ Code/0Pascal_S_testcode/test2.pas
@@ -0,0 +1,8 @@
+{type类型定义与应用}
+program test(input, output);
+type a=integer;b=real; 
+var s:a; z:b;
+begin
+    s:=1;
+    b:=0.01
+end.
---
+++ Code/0Pascal_S_testcode/test3.pas
@@ -0,0 +1,8 @@
+{record声明与引用}
+program test(input, output);
+type rec=record a:integer; b:real end; 
+var s:rec; z:real;
+begin
+    s.a:=1;
+    s.b:=z
+end.
