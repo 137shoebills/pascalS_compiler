1f069d926839e8b2082bb78a9cd269777424c567
Nimloth D <57124280+lxl23333@users.noreply.github.com>
2022-05-19 19:08:17 +0800

    add 数组变量对应的LLVM Type；getVarllType()

--- Code/5CodeGeneration/TypeSystem.cpp
+++ Code/5CodeGeneration/TypeSystem.cpp
@@ -15,12 +15,20 @@ llvm::Type* TypeSystem::getllType(string type){
             return this->boolTy;
         case "void":
             return this->voidTy;
+        case "int64":
+            return this->int64Ty;
+        default:
+        {
+            //数组类型(这里type是数组变量的名称)
+            if (this->arrayTypes.find(type) != this->arrayTypes.end())
+                return this->arrayTypes[type];
+            // record类型
+            else if (this->recordTypes.find(type) != this->recordTypes.end())
+                return this->recordTypes[type];
+            else
+                return nullptr;
+        }
     }
-    //record类型
-    if(this->recordTypes.find(type) != this->recordTypes.end())
-        return this->recordTypes[type];
-
-    return nullptr;
 }
 
  //新增record对应的LLVM类型
@@ -50,4 +58,31 @@ long TypeSystem::getRecordMemberIndex(string recName, string memName){
     //报错：未知的record成员
     semanticErrorInformation.push_back("Error: Undefined record member: " + recName + "." + memName);
     return 0;
+}
+
+//新增数组变量对应的LLVM类型
+void TypeSystem::addArrayType(string name, llvm::ArrayType* type){
+    arrayTypes[name] = type;
+    //arrayMembers[name] = vector<NameType>();
+}
+
+//获取变量对应的LLVM Type(普通变量，数组元素，record成员，函数标识符)
+llvm::Type* TypeSystem::getVarllType(_VariantReference* varRef){
+    int loc = mainSymbolTable->idToLoc[varRef->variantId.first].top();
+    _SymbolRecord* var = mainSymbolTable->recordList[loc];
+    llvm::Type* type = nullptr;
+    
+    if(var->records.size() > 0){   //record成员
+        string vpartId = varRef->IdvpartList[0]->IdvpartId.first;
+        for(auto it = var->records.begin(); it != var->records.end(); it++)
+        {
+            if((*it)->id == vpartId)
+                type = getllType((*it)->type);
+                break;
+        }
+    }
+    else    //普通变量/数组元素/函数返回值类型
+        type = getllType(var->type);
+
+    return type;
 }
