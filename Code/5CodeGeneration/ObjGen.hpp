58de458f68f148b78c11fe1d044a2ef8e7e21ad9
Nimloth D <57124280+lxl23333@users.noreply.github.com>
2022-05-25 21:17:37 +0800

    修改了一些bug

--- Code/5CodeGeneration/ObjGen.hpp
+++ Code/5CodeGeneration/ObjGen.hpp
@@ -22,8 +22,6 @@
 
 using namespace std;
 
-void ObjCodeGen(CodeGenContext& context, string& filename);
-
 void ObjCodeGen(CodeGenContext& context, string& filename, int outputType) {
     //初始化llvm，设置目标机
     llvm::InitializeAllTargetInfos();
@@ -33,14 +31,14 @@ void ObjCodeGen(CodeGenContext& context, string& filename, int outputType) {
     llvm::InitializeAllAsmPrinters();
 
     //获取目标三元组并设置
-    auto targetTriple = sys::getDefaultTargetTriple();
+    auto targetTriple = llvm::sys::getDefaultTargetTriple();
     context.module->setTargetTriple(targetTriple);
 
     //获取目标机数据类型并设置
     string TargetError;
     auto target = llvm::TargetRegistry::lookupTarget(targetTriple, TargetError);
     if(!target){
-        errs()<<TargetError;
+        llvm::errs()<<TargetError;
         return;
     }
     auto CPU = "generic";
@@ -48,21 +46,23 @@ void ObjCodeGen(CodeGenContext& context, string& filename, int outputType) {
 
     llvm::TargetOptions options;
     auto relocationModel = llvm::Reloc::Model::PIC_;
-    auto theTargetMachine = target->createTargetMachine(TargetTriple, CPU, features, options, relocationModel);
+    auto theTargetMachine = target->createTargetMachine(targetTriple, CPU, features, options, relocationModel);
     context.module->setDataLayout(theTargetMachine->createDataLayout());
-    
+    context.module->setTargetTriple(targetTriple);
+
     //将目标代码输出到文件
     error_code ErrCode;
     llvm::raw_fd_ostream dest(filename, ErrCode, llvm::sys::fs::OF_None);
     llvm::legacy::PassManager pass;
 
+    llvm::CodeGenFileType type;
     if(outputType == ASSEMBLY_FILE){    //生成汇编文件
-        //llvm::CodeGenFileType type = llvm::CGFT_AssemblyFile;
-        llvm::LLVMTargetMachine::CodeGenFileType type = llvm::LLVMTargetMachine::CGFT_AssemblyFile;
+        type = llvm::CGFT_AssemblyFile;
+        //llvm::LLVMTargetMachine::CodeGenFileType type = llvm::LLVMTargetMachine::CGFT_AssemblyFile;
     }
     else if(outputType == OBJECT_FILE){ //生成目标文件
-        //llvm::CodeGenFileType type = llvm::CGFT_ObjectFile;
-        llvm::LLVMTargetMachine::CodeGenFileType type = llvm::LLVMTargetMachine::CGFT_ObjectFile;
+        type = llvm::CGFT_ObjectFile;
+        //llvm::LLVMTargetMachine::CodeGenFileType type = llvm::LLVMTargetMachine::CGFT_ObjectFile;
     }
     else{
         cout<<"UNSUPPORTED output file format"<<endl;
@@ -70,11 +70,13 @@ void ObjCodeGen(CodeGenContext& context, string& filename, int outputType) {
     }
 
     theTargetMachine->addPassesToEmitFile(pass, dest, nullptr, type);
-    pass.run(*context.module.get());
+    pass.run(*(context.module));
     dest.flush();
 
     //打印LLVM IR
     context.module->print(llvm::outs(), nullptr);
+
+    llvm::outs() << "Object code wrote to " << filename.c_str() << "\n";
 }
 
 #endif
