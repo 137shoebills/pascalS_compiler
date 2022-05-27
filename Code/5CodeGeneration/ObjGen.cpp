0046eb5fb232c402d4699367d8c4d253789b4843
Nimloth D <57124280+lxl23333@users.noreply.github.com>
2022-05-28 00:41:07 +0800

    Update ObjGen.cpp

--- Code/5CodeGeneration/ObjGen.cpp
+++ Code/5CodeGeneration/ObjGen.cpp
@@ -2,6 +2,7 @@
 #include "ObjGen.h"
 
 void ObjCodeGen(CodeGenContext& context, string& filename, int outputType) {
+    cout<<"ObjCodeGen"<<endl;
     //初始化llvm，设置目标机
     llvm::InitializeAllTargetInfos();
     llvm::InitializeAllTargets();
@@ -11,7 +12,7 @@ void ObjCodeGen(CodeGenContext& context, string& filename, int outputType) {
 
     //获取目标三元组并设置
     auto targetTriple = llvm::sys::getDefaultTargetTriple();
-    context.module->setTargetTriple(targetTriple);
+    context.Module->setTargetTriple(targetTriple);
 
     //获取目标机数据类型并设置
     string TargetError;
@@ -26,13 +27,12 @@ void ObjCodeGen(CodeGenContext& context, string& filename, int outputType) {
     llvm::TargetOptions options;
     auto relocationModel = llvm::Reloc::Model::PIC_;
     auto theTargetMachine = target->createTargetMachine(targetTriple, CPU, features, options, relocationModel);
-    context.module->setDataLayout(theTargetMachine->createDataLayout());
-    context.module->setTargetTriple(targetTriple);
+    context.Module->setDataLayout(theTargetMachine->createDataLayout());
+    context.Module->setTargetTriple(targetTriple);
 
     //将目标代码输出到文件
     error_code ErrCode;
     llvm::raw_fd_ostream dest(filename, ErrCode, llvm::sys::fs::OF_None);
-    llvm::legacy::PassManager pass;
 
     llvm::CodeGenFileType type;
     if(outputType == ASSEMBLY_FILE){    //生成汇编文件
@@ -43,17 +43,26 @@ void ObjCodeGen(CodeGenContext& context, string& filename, int outputType) {
         type = llvm::CGFT_ObjectFile;
         //llvm::LLVMTargetMachine::CodeGenFileType type = llvm::LLVMTargetMachine::CGFT_ObjectFile;
     }
-    else{
-        cout<<"UNSUPPORTED output file format"<<endl;
+
+    llvm::legacy::PassManager pass;
+    if(theTargetMachine->addPassesToEmitFile(pass, dest, nullptr, type)){
+        llvm::errs() << "theTargetMachine can't emit a file of this type";
         return;
     }
+    //cout<<"here"<<endl;
 
-    theTargetMachine->addPassesToEmitFile(pass, dest, nullptr, type);
-    pass.run(*(context.module));
+    //SemanticAnalyseProgram最后的PM:成功输出到终端
+    // llvm::legacy::PassManager pm;
+	// pm.add(llvm::createPrintModulePass(llvm::outs()));
+	// pm.run(*(context.Module));
+
+    pass.run(*(context.Module));
+    cout<<"here2"<<endl;
     dest.flush();
+    cout<<"here3"<<endl;
 
     //打印LLVM IR
-    context.module->print(llvm::outs(), nullptr);
+    context.Module->print(llvm::outs(), nullptr);
 
     llvm::outs() << "Object code wrote to " << filename.c_str() << "\n";
 }
