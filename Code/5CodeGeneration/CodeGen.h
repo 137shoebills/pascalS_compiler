bd42d06ec005e76e25bb3b382aa630fce0a04e68
Nimloth D <57124280+lxl23333@users.noreply.github.com>
2022-05-31 22:37:47 +0800

    删除了一些注释

--- Code/5CodeGeneration/CodeGen.h
+++ Code/5CodeGeneration/CodeGen.h
@@ -20,12 +20,6 @@
 #include "llvm/Support/TargetSelect.h"
 
 #include <llvm/Support/raw_ostream.h>
-// #include "llvm/Support/TargetSelect.h"
-// #include "llvm/Support/TargetRegistry.h"
-// #include "llvm/Support/TargetSelect.h"
-// #include "llvm/Target/TargetMachine.h"
-// #include "llvm/Target/TargetOptions.h"
-// #include "llvm/ExecutionEngine/ExecutionEngine.h"
 
 #include <stdio.h> //LogError: fprintf
 #include <iostream>
@@ -37,13 +31,10 @@
 #include "ASTnodes.h"
 #include "symbolTable.h"
 #include "TypeSystem.h"
-//#include "yacc.tab.h"
-//#include "yacc.tab.cpp"
 
 #define ISTYPE(value, id) (value->getType()->getTypeID() == id)
 
 using namespace std;
-// using legacy::PassManager;
 
 class CodeGenContext;
 
@@ -51,45 +42,39 @@ extern bool have_error;
 
 extern CodeGenContext context;
 extern _SymbolTable *mainSymbolTable;
-extern _SymbolRecord *findSymbolRecord(string id);
+extern _SymbolRecord* findSymbolRecord(string id);
 
 extern string itos(int num); //将int转化为string
 
-class CodeGenContext
-{
+class CodeGenContext {
 public:
     llvm::LLVMContext llvmContext;
-    // llvm::IRBuilder<> builder;
     unique_ptr<llvm::IRBuilder<>> builder;
     unique_ptr<llvm::Module> Module;
-    TypeSystem typeSystem; //自定义类型系统
+    TypeSystem typeSystem;    //自定义类型系统
 
-    // CodeGenContext(): builder(llvmContext), typeSystem(llvmContext) {
-    CodeGenContext() : typeSystem(llvmContext)
-    {
-        // Module = make_unique<llvm::Module>("Program", this->llvmContext);
+    CodeGenContext(): typeSystem(llvmContext) {
+        //Module = make_unique<llvm::Module>("Program", this->llvmContext);
         Module = make_unique<llvm::Module>("main", this->llvmContext);
-        // builder = make_unique<llvm::IRBuilder<>>(llvmContext);
     }
-
+    //初始化库函数
     void InitCodeGen();
 };
 
 //用于报错的函数：
-// unique_ptr<_Expression> LogError(const char *str)
-llvm::Value *LogError(const char *str);
-llvm::Value *LogErrorV(string str);
+llvm::Value* LogError(const char *str);
+llvm::Value * LogErrorV(string str);
 
 //获取数组元素的指针
-llvm::Value *getArrayItemPtr(string varType, llvm::Value *addr, int loc);
+llvm::Value* getArrayItemPtr(string varType, llvm::Value* addr, int loc);
 
 //获取record成员的指针
-llvm::Value *getRecordItemPtr(string varType, llvm::Value *addr, string memberId);
+llvm::Value* getRecordItemPtr(string varType, llvm::Value* addr, string memberId);
 
 //获取数组元素/record成员的指针     (调用getArrayItemPtr和getRecordItemPtr)
-llvm::Value *getItemPtr(_VariantReference *varRef);
+llvm::Value* getItemPtr(_VariantReference* varRef);
 
 //计算N维数组下标（N>=1）   (按C标准，从0开始)
-int calcArrayIndex(string arrTypeName, vector<_Idvpart *> IdvpartList);
+int calcArrayIndex(string arrTypeName, vector<_Idvpart*> IdvpartList);
 
 #endif
